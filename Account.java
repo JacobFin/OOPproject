b3254342fd73a525decc78260270cd9828144445
JacobFin <44954274+JacobFin@users.noreply.github.com>
2018-11-16 15:45:19 -0600

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,18 @@
+
+package project;
+
+
+public class Account 
+{
+    int Account_Number;
+    int PIN_Number;
+    int SSN;
+    int Balance;
+    Account(int w,int x,int y,int z)
+    {
+        Account_Number = w;
+        PIN_Number = x;
+        SSN = y;
+        Balance = z;
+    }
+}
